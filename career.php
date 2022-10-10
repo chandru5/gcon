c89e53d433949c6729bde647a86749a2ba279e4f
chandrasekaran <chandrasekaran.p@live.com>
2022-10-10 13:56:51 +0530

    email attachment issue fixing

--- career.php
+++ career.php
@@ -1,6 +1,6 @@
 <?php 
-    ini_set( 'display_errors', 1);
-    error_reporting( E_ALL );
+    ini_set( 'display_errors', 0);
+    // error_reporting( E_ALL );
     include('database.php');
     if(isset($_REQUEST['action']) && $_REQUEST['action']=='save') {
         $returnArray = [];
@@ -10,6 +10,31 @@
         $message = "Please find the attached details";
         $headers = "From:" . $from;
         $name = date('ydms').basename($_FILES["resume"]["name"]);
+
+         //read from the uploaded file & base64_encode content
+        $size       =  $_FILES['resume']['size'];
+        $handle     = fopen($_FILES['resume']['tmp_name'], "r"); // set the file handle only for reading the file
+        $content    = fread($handle, $size); // reading the file
+        fclose($handle);                 // close upon completion
+    
+        $encoded_content = chunk_split(base64_encode($content));
+        
+        $boundary = md5("random"); // define boundary with a md5 hashed 
+
+        //plain text
+        $body = "--$boundary\r\n";
+        $body .= "Content-Type: text/plain; charset=ISO-8859-1\r\n";
+        $body .= "Content-Transfer-Encoding: base64\r\n\r\n";
+        $body .= chunk_split(base64_encode($message));
+            
+        //attachment
+        $body .= "--$boundary\r\n";
+        $body .="Content-Type: $type; name=".$name."\r\n";
+        $body .="Content-Disposition: attachment; filename=".$name."\r\n";
+        $body .="Content-Transfer-Encoding: base64\r\n";
+        $body .="X-Attachment-Id: ".rand(1000, 99999)."\r\n\r\n";
+        $body .= $encoded_content; // Attaching the encoded file with email
+        
         if(move_uploaded_file($_FILES['resume']['tmp_name'], './upload/'.$name)) {
 
             $data = [
@@ -216,8 +241,7 @@
                     email: true
                 },
                 resume: {
-                    required: true,
-                    extension: "pdf|docx|doc|PDF|DOCX|DOC"
+                    required: true
                 }
             },
             messages: {
@@ -227,8 +251,7 @@
                     number: "Number only allowed",
                 },
                 resume: {
-                    required: 'Upload your resume',
-                    extension: 'Docx, PDF only allowed should be below 2MB'
+                    required: 'Upload your resume'
                 },
                 email_address: "Please enter a valid email address",
             },
@@ -257,6 +280,7 @@
                     success: function(res) {
                         $('.submit-error').hide();
                         $('.submit-success').hide();
+                        res = $.parseJSON(res);
                         if (res.status == 'success') {
                             $('.submit-success').show();
                         } else {
